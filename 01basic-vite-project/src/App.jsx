d730a184c4aa933a549e18d8d8f99ed81f2748ca
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-20 14:20:17 +0530

    v2: learned to make components in react

--- 01basic-vite-project/src/App.jsx
+++ 01basic-vite-project/src/App.jsx
@@ -1,7 +1,13 @@
+import HelloWorld from "./hw" 
+
 
 function App() {
   return (
+    // fragments -> <></> because vite doesnt allow two or more elements so all elements are wrapped in single element -> fragment
+    <> 
+    <HelloWorld/>
     <h1>shubham rout working with vite</h1>
+    </>
   )
 }
 
---
+++ 01basic-vite-project/src/hw.jsx
@@ -0,0 +1,7 @@
+function HelloWorld(){ //function naming should be capitalized
+  return(
+    <h1>hello world!</h1>
+  )
+}
+
+export default HelloWorld;

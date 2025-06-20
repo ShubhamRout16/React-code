e4e3c81da905f03d7ab9b53da337ee8401bd596f
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-20 22:12:13 +0530

    v7 : asked interview qs

--- 02countervite/src/App.jsx
+++ 02countervite/src/App.jsx
@@ -10,7 +10,21 @@ function App() {
     if(counter > 20){
       setCounter(counter + 0)
     }else if(counter < 20 && counter >= 0){
-      setCounter(counter + 1)
+      //  interview asked qs -> setCounter is executed so many times will value change or work the same way 
+      // -> will work normally because its the property of useState hooks that it sends command to fibre in batches so all setCounter are seen as same task by fibre
+      // its also called snapshot state
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      // will add continously each of the previous results
     }
   }
 

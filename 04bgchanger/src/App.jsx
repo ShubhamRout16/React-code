a912120dcc31edb8b738a690cb8b42a4c4d15ea3
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-21 12:55:27 +0530

    v8(2.0) : optimised method for bg changer

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,30 +6,40 @@ import './App.css'
 function App() {
   const [color,setColor] = useState('red');
 
-  const Blue = () => {
-    setColor('blue');
-  }
-  const black = () => {
-    setColor('black');
-  }
-  const green = () => {
-    setColor('green');
-  }
-  const olive = () => {
-    setColor('olive');
-  }
+  // const Blue = () => {
+  //   setColor('blue');
+  // }
+  // const black = () => {
+  //   setColor('black');
+  // }
+  // const green = () => {
+  //   setColor('green');
+  // }
+  // const olive = () => {
+  //   setColor('olive');
+  // }
   
 
-  return (
-    <div className='w-screen h-screen' 
+  // return (
+  //   <div className='w-screen h-screen' 
+  //   style={{backgroundColor: color}}>
+  //     <h1>bg changer</h1>
+  //     <button onClick={Blue}>blue</button>
+  //     <button onClick={black}>black</button>
+  //     <button onClick={green}>green</button>
+  //     <button onClick={olive}>olive</button>
+  //   </div>
+  // )
+
+  // better method -> we are not repeating ourself
+  <div className='w-screen h-screen' 
     style={{backgroundColor: color}}>
       <h1>bg changer</h1>
-      <button onClick={Blue}>blue</button>
-      <button onClick={black}>black</button>
-      <button onClick={green}>green</button>
-      <button onClick={olive}>olive</button>
+      <button onClick={() => {setColor('blue')}}>blue</button>
+      <button onClick={() => {setColor('black')}}>black</button>
+      <button onClick={() => {setColor('green')}}>green</button>
+      <button onClick={() => {setColor('olive')}}>olive</button>
     </div>
-  )
 }
 
 export default App
